6f40c18f4b4911da665107df29c6c8cd78ec89c2
ait <ait@ex.in>
2020-08-26 15:12:32 +0900

    [API] get api test / view memos use axios

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,46 +1,84 @@
 <template>
-<div id="app">
-  <p>{{ message }}</p>
-  <!-- <p>{{ $data }}</p> -->
-  <ul>
-    <li v-for="memo in memos" :key="memo">
-      {{ memo.title }} :
-      {{ memo.description }}
-    </li>
-  </ul>
-</div>
+  <div id="app">
+    <!-- <p>{{ $data }}</p> -->
+    <div class="memo_lay">
+      <h2>Memo API TEST</h2>
+      <ul class="memo_org">
+        <li v-for="memo in memos" :key="memo" class="memo_mol">
+          <div class="memo_title">{{ memo.title }}</div>
+          <div class="memo_description">{{ memo.description }}</div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!",
+      // init
       memos: ''
-    }
+    };
   },
   mounted() {
-    this.setMemo()
+    // definition Loading
+    this.setMemo();
   },
   methods: {
-    setMemo: function () {
-      axios.get('/api/memos')
+    setMemo() {
+      axios
+        .get('/api/memos')
         .then(res => {
           this.memos = res.data
         })
         .catch(err => {
-          console.error("err");
-        })
+          console.error('err api')
+          // FIXME: ↓Local test message for develop, Don't deploy!
+          document.write('APIエラー')
+        });
     }
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style lang="scss" scoped>
+$bold: 600;
+#app {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color: #34393c;
+}
+
+.memo {
+  &_lay {
+    // max-width: 800px;
+    margin: auto;
+  }
+  &_org {
+    overflow-x: auto;
+    max-height: 320px;
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+    list-style: none;
+    border: 1px solid gray;
+    padding: 1.5rem;
+  }
+  &_mol {
+    display: flex;
+    font-size: 1.25rem;
+    border-bottom: 1px solid #ccc;
+    line-height: 2;
+  }
+  &_title {
+    width: 120px;
+    overflow-x: auto;
+    margin-right: 12px;
+    font-weight: $bold;
+  }
 }
 </style>
