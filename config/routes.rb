a538d9dc7291c6661450f179e969a3ae23300b0b
ait <ait@ex.in>
2020-08-26 15:13:35 +0900

    [POST] add post / :create on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'home#index'
 
   namespace :api, format: 'json' do
-    resources :memos, only: [:index]
+    resources :memos, only: [:index, :create]
   end
 end
